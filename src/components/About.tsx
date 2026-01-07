1422adbff5441d44394967d723124727871e443b
kwonjin2 <rnjswls060@gmail.com>
2026-01-07 13:29:53 +0900

    fix: 이미지 비율 수정

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -5,30 +5,28 @@ export default function About({ isVisible }: { isVisible: boolean }) {
   return (
     <section
       id="about"
-      className="min-h-screen flex flex-col items-center justify-center px-6 border-b border-gray-300"
+      className="min-h-screen flex flex-col items-center justify-center px-6 border-b border-gray-300 gap-10"
     >
       <motion.div
-        className="relative w-[180px] h-[250px] md:w-[220px] md:h-[310px] lg:w-[263px] lg:h-[368px] xl:w-[300px] xl:h-[420px] 2xl:w-[360px] 2xl:h-[500px]"
+        className="relative w-[180px] aspect-[3/4] md:w-[220px] lg:w-[263px] xl:w-[300px] 2xl:w-[360px] overflow-hidden rounded-4xl"
         initial={{ rotateY: 90, opacity: 0 }}
         animate={
           isVisible ? { rotateY: 0, opacity: 1 } : { rotateY: 90, opacity: 0 }
         }
         transition={{ duration: 0.8, ease: "easeOut" }}
       >
-        <div className="rounded-4xl">
-          <Image
-            src="/profile.jpeg"
-            alt="profile"
-            fill
-            sizes="(max-width: 768px) 180px,
+        <Image
+          src="/profile.jpeg"
+          alt="profile"
+          fill
+          sizes="(max-width: 768px) 180px,
                  (max-width: 1024px) 220px,
                  (max-width: 1280px) 263px,
                  (max-width: 1536px) 300px,
                  360px"
-            className="object-contain"
-            priority
-          />
-        </div>
+          className="object-cover"
+          priority
+        />
       </motion.div>
 
       <p className="mt-3 text-[15px] md:text-[20px] lg:text-[25px] 2xl:text-[40px] text-slate-400 text-center">
